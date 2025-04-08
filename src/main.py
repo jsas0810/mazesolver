136d30ec5d11c275ec369996a865b4a5ab203dd8
jsas0810 <jsas0810@gmail.com>
2025-04-08 11:39:13 -0400

    donezo

--- src/main.py
+++ src/main.py
@@ -1,26 +1,28 @@
-from window import Window, Line, Point
-from cell import Cell
+from window import Window
+from maze import Maze
+import sys
+
 
 def main():
-    win = Window(800, 600)
-    
-    c = Cell(win)
-    c.has_left_wall = False
-    c.draw(50, 50, 100, 100)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
 
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(125, 125, 200, 200)
+    sys.setrecursionlimit(10000)
+    win = Window(screen_x, screen_y)
 
-    c = Cell(win)
-    c.has_bottom_wall = False
-    c.draw(225, 225, 250, 250)
-
-    c = Cell(win)
-    c.has_top_wall = False
-    c.draw(300, 300, 500, 500)
-
-
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
+    print("maze created")
+    is_solvable = maze.solve()
+    if not is_solvable:
+        print("maze can not be solved!")
+    else:
+        print("maze solved!")
     win.wait_for_close()
+
 
 main()
